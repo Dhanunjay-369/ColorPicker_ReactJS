f7d999f14e28e34f820a4d083a5fae1fff6a9e9d
Dhanunjay <dhanunjayareddykurakula@gmail.com>
2023-10-14 23:50:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import Button from 'react-bootstrap/Button';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Color } from './components/pickColor.js';
+
+function App() {
+  return (
+    <Color/>
+  );
+}
+
+export default App;
